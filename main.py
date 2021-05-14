f64b862715cf87c7da83c214d3e72f50bdc3e807
Akshansh Bhanjana <akshansh2000@gmail.com>
2021-05-14 18:03:57 +0530

    add torrent and file fetching

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ author: Akshansh Bhanjana (akshansh2000@gmail.com)
 """
 
 import os
+import sys
+import json
 
-import requests
-import questionary
+from requests import get, post, exceptions
+from questionary import rawselect
 
 
 BASE_URL = "https://api.real-debrid.com/rest/1.0"
@@ -18,12 +20,22 @@ AUTH_HEADERS = {
     "Authorization": "Bearer " + os.environ["DEBRID_TOKEN"],
 }
 
+TORRENTS_LIST = None
 
-def get_torrents_list() -> list:
+
+def get_torrents_list() -> None:
     """
     retrieve list of torrents from debrid (max: 100)
     """
-    pass
+    response = json.loads(
+        get(f"{BASE_URL}/torrents", headers=AUTH_HEADERS).text
+    )
+
+    global TORRENTS_LIST
+    TORRENTS_LIST = [{
+        "id": torrent['id'],
+        "name": torrent['filename'],
+    } for torrent in response]
 
 
 def get_files_list(torrent_id: str) -> list:
@@ -32,7 +44,26 @@ def get_files_list(torrent_id: str) -> list:
 
     ! filters files which weren't added to host
     """
-    pass
+    response = json.loads(
+        get(
+            f"{BASE_URL}/torrents/info/{torrent_id}",
+            headers=AUTH_HEADERS,
+        ).text
+    )
+
+    files_list = []
+
+    index = 0
+    for file_info in response["files"]:
+        if file_info["selected"]:
+            files_list.append({
+                "name": file_info["path"][1:],
+                "hoster_link": response["links"][index]
+            })
+
+            index += 1
+
+    return files_list
 
 
 def unrestrict_link(hoster_link: str) -> str:
